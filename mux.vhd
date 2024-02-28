ba2faacc68fc9b53e50e62e38cd192ec83d03d68
AlessandroDF <adf.alessandrodelfatti@gmail.com>
2024-02-28 14:44:13 +0100

    Sistemato errore di dimensione in mux. Scritto codice di w_reg

--- mux.vhd
+++ mux.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 entity mux is
     port(
         in_w        : in std_logic_vector(7 downto 0);
-        in_c        : in std_logic_vector(7 downto 0);
+        in_c        : in std_logic_vector(4 downto 0);
         en_mux      : in std_logic;
         sel_mux     : in std_logic;
         i_clk       : in std_logic;
@@ -15,15 +15,19 @@ entity mux is
 end entity mux;
 
 architecture mux_arch of mux is
+    signal stored_w : std_logic_vector(7 downto 0);
+    signal stored_c : std_logic_vector(4 downto 0);
 begin
+    in_w <= stored_w;
+    in_c <= stored_c;
     process(i_clk, en_mux, sel_mux)
     begin
         o_mem_data <= (others => '0');
         if i_clk'event AND i_clk = '1' then
             if en_mux = '1' then
                 case sel is
-                    when '1' => o_mem_data <= in_w;
-                    when '0' => o_mem_data <= in_c;
+                    when '1' => o_mem_data <= stored_w;
+                    when '0' => o_mem_data <= "000" & stored_c;
                 end case;
             end if;
         end if;
--- w_reg.vhd
+++ w_reg.vhd
@@ -8,13 +8,39 @@ entity w_reg is
        i_mem_data   : in std_logic_vector(7 downto 0);
        i_clk        : in std_logic;
        i_rst        : in std_logic;
+       i_first_val  : in std_logic;
        en_w_update  : in std_logic;
        
        out_w        : out std_logic_vector(7 downto 0);
-       out_c        : out std_logic_vector(7 downto 0)
+       out_c        : out std_logic_vector(4 downto 0)
     );
 end entity w_reg;
 
 architecture w_reg_arch of w_reg is
+    signal stored_w : std_logic_vector(7 downto 0);
+    signal stored_c : std_logic_vector(4 downto 0);
 begin
+    out_w <= stored_w;
+    out_c <= stored_c;
+    process(i_clk, i_rst)
+        if i_clk'event AND i_clk = '1' then
+            if i_mem_data = "00000000" then
+                if en_w_update = '1' then
+                    if i_first_val = '1' then
+                        stored_w <= i_mem_data;
+                        stored_c <= "00000";
+                    elsif i_firs_val = '0' then
+                        -- Non aggiorno stored_w perchè deve mantenere il
+                        -- valore aveva precedentemente
+                        if stored_c > "00000" then
+                            stored_c <= stored_c - "00001";
+                        end if;
+                    end if;
+                end if;
+            elsif i_mem_data > "00000000" then
+                -- Lascio stored_w al suo valore precedente
+                stored_c <= "11111";
+            end if;
+        end if;
+    end process;
 end architecture w_reg_arch;
